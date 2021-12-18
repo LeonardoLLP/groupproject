7a0a28d026dcf289e2af8cefbfaf97a420d409ed
migueliiin <91721552+migueliiin@users.noreply.github.com>
2021-12-18 13:49:07 +0100

    Finalizado

--- La_Escalera/La_Escalera.py
+++ La_Escalera/La_Escalera.py
@@ -14,11 +14,13 @@ def staircase(n):
     for i in range(1,n+1):
         #print(" "*(n-i),end="")
         # imprimir blancos
-        for j in range(0,n-i):
+        for j in range(0,int((n-i)/2)):
             print(" ",end="")
         for k in range(0,i):
             print("#",end="")
         print()
+        #solo se podria hacer como en el ejemplo mostrado en el enunciado
+        #si se pudiesen usar medios caracteres
 
 if __name__ == '__main__':
     n = int(input().strip())
