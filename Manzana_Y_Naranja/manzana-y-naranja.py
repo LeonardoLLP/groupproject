d2bd46d0eb05424c5a09fa9ef320dc88a7d22401
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2021-12-19 00:45:03 +0100

    Actualizado codigo inicial de manzana-y-naranja

--- Manzana_Y_Naranja/manzana-y-naranja.py
+++ Manzana_Y_Naranja/manzana-y-naranja.py
@@ -22,12 +22,11 @@ if __name__ == '__main__':
     first_multiple_input = input("House x1 and x2: ").rstrip().split()
     s = int(first_multiple_input[0])
     t = int(first_multiple_input[1])
-    second_multiple_input = input().rstrip().split()
+    second_multiple_input = input("Appler and oranger: ").rstrip().split()  #! XD
     a = int(second_multiple_input[0])
     b = int(second_multiple_input[1])
-    third_multiple_input = input().rstrip().split()  #! Para qué sirve esto???
-    m = int(third_multiple_input[0])
-    n = int(third_multiple_input[1])
-    apples = list(map(int, input().rstrip().split()))
-    oranges = list(map(int, input().rstrip().split()))
+
+    apples = list(map(int, input("Apples: ").rstrip().split()))
+    oranges = list(map(int, input("Oranges: ").rstrip().split()))
+
     countApplesAndOranges(s, t, a, b, apples, oranges)
